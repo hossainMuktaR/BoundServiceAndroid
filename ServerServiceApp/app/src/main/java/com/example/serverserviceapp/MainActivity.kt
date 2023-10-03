011389336a73fe4486bb5b6eed8117657160ea68
Hossain MuktaR <HossainMuktarDev@gmail.com>
2023-10-04 01:59:19 +0600

    added AIDL random Number service feature

--- ServerServiceApp/app/src/main/java/com/example/serverserviceapp/MainActivity.kt
+++ ServerServiceApp/app/src/main/java/com/example/serverserviceapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.serverserviceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.serverserviceapp.ui.theme.ServerServiceAppTheme
@@ -22,25 +24,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center){
+                        Text("Hello World")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ServerServiceAppTheme {
-        Greeting("Android")
     }
 }
